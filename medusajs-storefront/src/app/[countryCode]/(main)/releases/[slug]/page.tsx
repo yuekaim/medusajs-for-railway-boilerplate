cdcd91a49d385d5bb6ec02a194bf5be8741c5a50
yuekaim <yuekaim@gmail.com>
2024-08-06 22:48:59 +0200

    take out unfinished pages

--- medusajs-storefront/src/app/[countryCode]/(main)/releases/[slug]/page.tsx
+++ medusajs-storefront/src/app/[countryCode]/(main)/releases/[slug]/page.tsx
@@ -1,5 +1,23 @@
-export default function Release() {
+import type { Release as ReleaseType } from '../../../../../../sanity/types/sanity.types';
+import { groq } from 'next-sanity';
+import {client} from '../../../../../../sanity/lib/client';
+import Image from 'next/image';
+import { urlFor } from '../../../../../../sanity/lib/image';
+
+const RELEASE_QUERY = groq`*[_type == "release" && slug.current == $slug][0]`
+
+export default async function Release() {
+    const release = await client.fetch<ReleaseType>(RELEASE_QUERY);
     return(
-        <div>release</div>
+        <div>
+            <div>
+                hello
+                {/* <Image
+                    src={urlFor(release.covorImage?.asset).url()}
+                    alt={release.covorImage?.caption}
+                /> */}
+            </div>
+            <div></div>
+        </div>
     )
 }
