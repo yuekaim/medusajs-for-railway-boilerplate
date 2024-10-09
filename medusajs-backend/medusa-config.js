d21e10ff2b4ffae58a718971f6d9965f5be9c8c8
yuekaim <yuekaim@gmail.com>
2024-10-08 20:24:38 -0400

    add stripe back

--- medusajs-backend/medusa-config.js
+++ medusajs-backend/medusa-config.js
@@ -72,13 +72,13 @@ const plugins = [
       },
     },
   },
-  // {
-  //   resolve: `medusa-payment-stripe`,
-  //   options: {
-  //     api_key: process.env.STRIPE_API_KEY,
-  //     webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
-  //   },
-  // },
+  {
+    resolve: `medusa-payment-stripe`,
+    options: {
+      api_key: process.env.STRIPE_API_KEY,
+      webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
+    },
+  },
   {
     resolve: `medusa-payment-paypal`,
     options: {
