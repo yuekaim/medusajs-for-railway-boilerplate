cff19fe8758f9fc48f9211780a8bab6741bbda32
yuekaim <yuekaim@gmail.com>
2025-04-28 04:39:43 +0800

    checkout redirect

--- medusajs-storefront/src/modules/checkout/actions.ts
+++ medusajs-storefront/src/modules/checkout/actions.ts
@@ -153,7 +153,8 @@ export async function setAddresses(currentState: unknown, formData: FormData) {
   }
 
   redirect(
-    `/${formData.get("shipping_address.country_code")}/checkout?step=delivery`
+    // `/${formData.get("shipping_address.country_code")}/checkout?step=delivery`
+    `/checkout?step=delivery`
   )
 }
 
